0e5dc812cefdb197293029f7eca969a5e212865f
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2024-06-23 14:58:00 +0700

    Add AI search functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,59 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+const toggleAi = ref(true)
+const search = ref('')
+const results:any = ref({})
+const loading = ref(false)
+const baseUrl = 'https://insight-bee-backend.vercel.app'
+let debounceTimeout: NodeJS.Timeout | null = null;
 
-const toggleAi = ref(false)
+// fetch data search from database
+
+// fetch data from ai
+const fetchAi = async () => {
+    try {
+        const response = await fetch(baseUrl + '/api/knowledge/question', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'ngrok-skip-browser-warning': 'true'
+            },
+            body: JSON.stringify({
+                question: search.value
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        loading.value = false
+        results.value = data
+    } catch (error) {
+        console.log(error)
+        loading.value = false
+    }
+}
+
+// // search action
+// const searching = async (e:any) => {
+//     search.value = e.target.value
+
+//     fetchAi()
+// }
+
+// Watch for changes in the search ref with debounce
+watch(search, (newValue) => {
+    if (debounceTimeout) {
+        clearTimeout(debounceTimeout);
+    }
+    debounceTimeout = setTimeout(() => {
+        fetchAi();
+    }, 2000); // 2 seconds debounce time
+});
+
+// Search action
+const searching = (e: any) => {
+    loading.value = true
+    search.value = e.target.value
+}
+
 </script>
 
 <template>
@@ -23,12 +75,19 @@ const toggleAi = ref(false)
                 </label>
             </div>
         </div>
-        <input type="text" placeholder="Type to search" class="p-3 border w-1/3" />
+        <input type="text" placeholder="Type to search" class="p-3 border w-1/3" :value="search"
+            @input="searching($event)">
 
         <div class="mt-6 w-full flex flex-col items-center justify-center">
             <!-- result from ai -->
             <div class="w-1/2 border-2 border-slate-200 p-3">
-                <div class="flex gap-2">
+                <div v-if="loading" class="flex items-center justify-center">
+                    <h1>loading...</h1>
+                </div>
+                <div v-else-if="!results && !loading" class="flex items-center justify-center">
+                    <h1>data not found</h1>
+                </div>
+                <div v-else class="flex gap-2">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                             class="bi bi-stars text-yellow-500" viewBox="0 0 16 16">
@@ -39,23 +98,19 @@ const toggleAi = ref(false)
 
                     <div>
                         <p class="text-sm text-slate-800">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, reprehenderit dolor error
-                            tempora deleniti velit consectetur delectus repellendus, repudiandae qui quidem eos optio
-                            asperiores, rerum odio ut. Possimus unde soluta impedit asperiores libero, ad inventore
-                            blanditiis accusantium, eos assumenda iste deleniti cupiditate fugit deserunt est? Dolor
-                            dolorem blanditiis voluptas officiis?
+                            {{ results.data?.answer }}
                         </p>
 
                         <!-- <small>
                             Generate by ai, some 
                         </small> -->
-                        <div class="mt-3">
+                        <div class="mt-3" v-if="results.data?.references.length > 0">
                             <label for="" class="text-sm">
                                 References
                             </label>
 
                             <div class="p-4 space-y-3">
-                                <div class="flex gap-2">
+                                <div v-for="(data, index) in results.data?.references" :key="index" class="flex gap-2">
                                     <div>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24"
                                             height="24" fill="currentColor" class="text-slate-500">
@@ -65,35 +120,19 @@ const toggleAi = ref(false)
                                     </div>
 
                                     <div>
-                                        <h2 class="text-sm font-semibold text-teal-500">Lorem, ipsum dolor.</h2>
-                                        <p class="text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                            Numquam, beatae?</p>
-                                    </div>
-                                </div>
-                                <div class="flex gap-2">
-                                    <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24"
-                                            height="24" fill="currentColor" class="text-slate-500">
-                                            <path
-                                                d="M168 80c-13.3 0-24 10.7-24 24V408c0 8.4-1.4 16.5-4.1 24H440c13.3 0 24-10.7 24-24V104c0-13.3-10.7-24-24-24H168zM72 480c-39.8 0-72-32.2-72-72V112C0 98.7 10.7 88 24 88s24 10.7 24 24V408c0 13.3 10.7 24 24 24s24-10.7 24-24V104c0-39.8 32.2-72 72-72H440c39.8 0 72 32.2 72 72V408c0 39.8-32.2 72-72 72H72zM176 136c0-13.3 10.7-24 24-24h96c13.3 0 24 10.7 24 24v80c0 13.3-10.7 24-24 24H200c-13.3 0-24-10.7-24-24V136zm200-24h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24z" />
-                                        </svg>
-                                    </div>
-
-                                    <div>
-                                        <h2 class="text-sm font-semibold text-teal-500">Lorem, ipsum dolor.</h2>
-                                        <p class="text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                            Numquam, beatae?</p>
+                                        <a :href="data" target="_blank" class="text-sm font-semibold text-teal-500">
+                                            {{ data }}
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
             <!-- result from database -->
-            <div class="w-1/2 border-2 border-slate-200 p-3">
+            <!-- <div class="w-1/2 border-2 border-slate-200 p-3">
                 <div class="p-4 space-y-3">
                     <div class="flex gap-2">
                         <div>
@@ -127,7 +166,7 @@ const toggleAi = ref(false)
                     </div>
                 </div>
 
-            </div>
+            </div> -->
         </div>
 
     </div>
